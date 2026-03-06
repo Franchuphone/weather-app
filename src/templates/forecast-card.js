882a7c08927a29720a11577fb733335ed14f29e6
Franchuphone <222453306+Franchuphone@users.noreply.github.com>
2026-03-06 22:08:39 +0100

    Fix temp cel to fah on all temps

--- src/templates/forecast-card.js
+++ src/templates/forecast-card.js
@@ -2,7 +2,7 @@ const forecastCard = `<div>
     <div class="forecast-day">{{day}}</div>
     <div class="forecast-icon"><img src="./img/{{icon}}.svg" alt="{{icon}} icon"></div>
     <div class="forecast-conditions">{{conditions}}</div>
-    <div class="forecast tempMaxMin">{{tempMin}} / {{tempMax}}</div>
+    <div class="forecast tempMaxMin"><span id="temp-number">{{tempMin}}</span> / <span id="temp-number">{{tempMax}}</span></div>
 </div>`
 
 export default forecastCard;
